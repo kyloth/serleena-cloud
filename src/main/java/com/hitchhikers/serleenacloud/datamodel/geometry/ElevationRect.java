7031801fa10f7a48672488c182785121ec62429c
Nicola Mometto <brobronsa@gmail.com>
2015-05-06 01:23:20 +0200

    DATAGEO: aggiungi ElevationRect
    Related to SHCLOUD-1

---
+++ src/main/java/com/hitchhikers/serleenacloud/datamodel/geometry/ElevationRect.java
@@ -0,0 +1,66 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+/**
+ * Name: ElevationRect.java
+ * Package: com.kyloth.serleenacloud.datamodel.geometry
+ * Author: Nicola Mometto
+ * Date: 2015-05-05
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0.0    Nicola Mometto      2015-05-05  Creazione file, codice e javadoc iniziali
+ */
+
+package com.kyloth.serleenacloud.datamodel.geometry;
+
+/**
+ * Rappresenta una generica area rettangolare di mappa delimitata da
+ * coordinate geografiche a cui e` associata un'altezza
+ *
+ * @author  Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ * @since   2015-05-06
+ */
+
+public class ElevationRect extends Rect implements IElevationRect {
+
+    private double height;
+
+
+    /**
+     * Costruisce un nuovo oggetto ElevationRect dati i due punti geografici degli angoli
+     * Nord-Ovest e Sud-Est e l'altezza associata
+     *
+     * @param Punto geografico dell'angolo Nord-Ovest
+     * @param Punto geografico dell'angolo Sud-Est
+     * @param Altezza associata
+     */
+
+    public ElevationRect(IPoint nw, IPoint se, double height) {
+        super(nw, se);
+        this.height = height;
+    }
+
+    /**
+     * Ritorna l'altezza associata con l'area rettangolare di mappa
+     *
+     * @return L'altezza associata
+     */
+
+    public double getHeight() {
+        return height;
+    }
+}
