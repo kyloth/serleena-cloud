fbe55ce30b32d2d010ce40af7d6415fbdffa33e8
Nicola Mometto <nicola.mometto@studenti.unipd.it>
2015-05-06 01:22:38 +0200

    DATAGEO: aggiungi Point
    Related to SHCLOUD-1

---
+++ src/main/java/com/kyloth/serleenacloud/datamodel/geometry/Point.java
@@ -0,0 +1,73 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+/**
+ * Name: Point.java
+ * Package: com.kyloth.serleenacloud.datamodel.geometry
+ * Author: Nicola Mometto
+ * Date: 2015-05-05
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0.0    Nicola Mometto      2015-05-05  Creazione file, codice e javadoc iniziali
+ */
+
+package com.kyloth.serleenacloud.datamodel.geometry;
+
+/**
+ * Rappresenta un generico punto geografico
+ *
+ * @author  Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ * @since   2015-05-05
+ */
+
+public class Point implements IPoint {
+    private double latitude;
+    private double longitude;
+
+
+    /**
+     * Crea un nuovo generico punto geografico, data latitudine e longitudine
+     *
+     * @param  latitude Coordinata latitudinale del punto da creare
+     * @param  longitude Coordinata longitudinale del punto da creare
+     */
+
+    public Point(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    /**
+     * Restituisce la latitudine del punto geografico
+     *
+     * @return Coordinata latitudinale
+     */
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * Restituisce la longitudine del punto geografico
+     *
+     * @return Coordinata longitudinale
+     */
+
+    public double getLongitude() {
+        return longitude;
+    }
+}
