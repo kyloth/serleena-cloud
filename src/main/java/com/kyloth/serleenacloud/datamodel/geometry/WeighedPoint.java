211e271411e3d8813de409653e26fbb2ef0b2195
Nicola Mometto <brobronsa@gmail.com>
2015-05-06 01:22:46 +0200

    DATAGEO: aggiungi WeighedPoint
    Related to SHCLOUD-1

---
+++ src/main/java/com/kyloth/serleenacloud/datamodel/geometry/WeighedPoint.java
@@ -0,0 +1,63 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+/**
+ * Name: WeighedPoint.java
+ * Package: com.kyloth.serleenacloud.datamodel.geometry
+ * Author: Nicola Mometto
+ * Date: 2015-05-05
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0.0    Nicola Mometto      2015-05-05  Creazione file, codice e javadoc iniziali
+ */
+
+package com.kyloth.serleenacloud.datamodel.geometry;
+
+/**
+ * Rappresenta un punto geografico a cui e` associata una piccola circonferenza
+ *
+ * @author  Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ * @since   2015-05-05
+ */
+
+public class WeighedPoint extends Point implements IWeighedPoint {
+    private double radius;
+
+
+    /**
+     * Crea un nuovo oggetto WeighedPoint data latitudine, longitudine e raggio associato
+     *
+     * @param Coordinata latitudinale del punto da creare
+     * @param Coordinata longitudinale del punto da creare
+     * @param Raggio associato al punto da creare
+     */
+
+    public WeighedPoint(double latitude, double longitude, double radius) {
+        super(latitude, longitude);
+        this.radius = radius;
+    }
+
+    /**
+     * Restituisce il raggio della circonferenza associata
+     *
+     * @return Raggio della circonferenza associata
+     */
+
+    public double getRadius() {
+        return radius;
+    }
+}
