53d88cadbfb36f8bf11db51568810302ce066907
Nicola Mometto <brobronsa@gmail.com>
2015-05-06 11:21:13 +0200

    DATABU: Aggiungi PointOfInterest
    Related to: SHCLOUD-3

---
+++ src/main/java/com/kyloth/serleenacloud/datamodel/business/PointOfInterest.java
@@ -0,0 +1,42 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.datamodel.business;
+
+import com.kyloth.serleenacloud.datamodel.geometry.Point;
+
+public class PointOfInterest extends Point {
+
+    public static enum POIType {
+        // TODO
+    }
+
+    String name;
+    POIType type;
+
+    PointOfInterest(double latitude, double longitude, String name, POIType type) {
+        super(latitude, longitude);
+        this.name = name;
+        this.type = type;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    POIType getPOIType() {
+        return type;
+    }
+}
