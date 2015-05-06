760c8bf33e5782a10e416909b51a1e8352d03df4
Nicola Mometto <brobronsa@gmail.com>
2015-05-06 11:19:46 +0200

    DATABU: Aggiungi CheckPoint
    Related to: SHCLOUD-3

---
+++ src/main/java/com/kyloth/serleenacloud/datamodel/business/CheckPoint.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.datamodel.business;
+
+import com.kyloth.serleenacloud.datamodel.geometry.Point;
+
+public class CheckPoint extends Point {
+
+    int id;
+
+    CheckPoint(double latitude, double longitude, int id) {
+        super(latitude, longitude);
+        this.id = id;
+    }
+
+    int getId() {
+        return id;
+    }
+}
