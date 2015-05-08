72240a2e3456e1fe5c7c5dfa8352494d37fc9d56
Nicola Mometto <nicola.mometto@studenti.unipd.it>
2015-05-08 13:16:24 +0200

    DATABU: Aggiungi ITelemetry
    Related to: SHCLOUD-3

---
+++ src/main/java/com/hitchhikers/serleenacloud/datamodel/business/ITelemetry.java
@@ -0,0 +1,20 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.datamodel.business;
+
+public interface ITelemetry extends Comparable<ITelemetry> {
+    Iterable<TelemetryEvent> getEvents();
+}
