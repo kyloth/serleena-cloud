81bd3348212b8d6c5e66c4d67039ba5a2edb8c30
Nicola Mometto <brobronsa@gmail.com>
2015-05-09 02:04:59 +0200

    DATABU: Aggiungi IWeatherForecast
    Related to: SHCLOUD-3

---
+++ src/main/java/com/kyloth/serleenacloud/datamodel/business/IWeatherForecast.java
@@ -0,0 +1,33 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.datamodel.business;
+
+import com.kyloth.serleenacloud.datamodel.geometry.IRect;
+
+import java.util.Date;
+
+public interface IWeatherForecast {
+
+    public static enum WeatherCondition {
+        // TODO
+    }
+
+    public WeatherCondition getForecast();
+    public double getTemperature();
+    public IRect getBoundingRect();
+    public Date getTime();
+
+}
