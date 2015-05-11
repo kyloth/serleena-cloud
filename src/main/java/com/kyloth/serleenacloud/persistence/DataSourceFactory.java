bdd20be86ee4bbf2dd3019cd7d8db0f3eb23897d
Nicola Mometto <nicola.mometto@studenti.unipd.it>
2015-05-11 04:01:05 +0200

    PERSISTENCE: Aggiungi DataSourceFactory
    Related to: SHCLOUD-10

---
+++ src/main/java/com/kyloth/serleenacloud/persistence/DataSourceFactory.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.persistence;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DataSourceFactory {
+    public static IDataSource getDataSource() {
+        ApplicationContext context = new ClassPathXmlApplicationContext("Spring-Module.xml");
+        return (IDataSource) context.getBean("dataSource");
+    }
+}
