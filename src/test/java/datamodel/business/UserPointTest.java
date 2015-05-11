94d971e3840323b0c96b55258f5197e1cae12d16
Gabriele Pozzan <fanofthepigpen@gmail.com>
2015-05-11 08:52:23 +0200

    TEST: aggiungi UserPointTest
    Related to: SHCLOUD-5

---
+++ src/test/java/datamodel/business/UserPointTest.java
@@ -0,0 +1,55 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+/**
+ * Name: UserPointTest.java
+ * Package: com.kyloth.serleenacloud.datamodel.business
+ * Author: Gabriele Pozzan
+ * Date: 2015-05-11
+ *
+ * History:
+ * Version  Programmer       Date        Changes
+ * 1.0.0    Gabriele Pozzan  2015-05-11  Creazione file e scrittura
+ *                                       codice e documentazione Javadoc
+ */
+
+package com.kyloth.serleenacloud.datamodel.business;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+
+/**
+ * Contiene i test di unità per la classe UserPoint.
+ *
+ * @author Gabriele Pozzan <gabriele.pozzan@studenti.unipd.it>
+ * @version 1.0.0
+ */
+
+public class UserPointTest {
+    /**
+     * Testa la correttezza del costruttore e dei metodi
+     * "getter" della classe.
+     */
+    @Test
+    public void testConstructor() {
+        double latitude = 11.50;
+        double longitude = 73.37;
+        String name = "Cirith Ungol";
+        UserPoint up = new UserPoint(latitude, longitude, name);
+
+        assertTrue(up.getName().equals("Cirith Ungol"));
+    }
+}
