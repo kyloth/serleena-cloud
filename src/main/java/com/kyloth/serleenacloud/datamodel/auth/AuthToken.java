7aca363192731325623a7b68c28558c34add7ebb
Gabriele Pozzan <nicola.mometto@studenti.unipd.it>
2015-05-12 09:55:47 +0200

    JAVADOC: aggiungi javadoc a AuthToken
    Related to: SHCLOUD-11

--- src/main/java/com/kyloth/serleenacloud/datamodel/auth/AuthToken.java
+++ src/main/java/com/kyloth/serleenacloud/datamodel/auth/AuthToken.java
@@ -13,12 +13,46 @@
 ******************************************************************************/
 
 
+/**
+ * Name: AuthToken.java
+ * Package: com.kyloth.serleenacloud.datamodel.auth
+ * Author: Nicola Mometto
+ * Date: 2015-05-05
+ *
+ * History:
+ * Version  Programmer          Date        Changes
+ * 1.0.0    Nicola Mometto      2015-05-05  Creazione file, codice e javadoc iniziali
+ */
+
 package com.kyloth.serleenacloud.datamodel.auth;
+
+/**
+ * Modella un token di autenticazione richiesto da alcune route, espone metodi
+ * con il quale è possibile verificare se il token autentica un utente.
+ *
+ * @author Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ */
 
 public class AuthToken {
 
+    /**
+     * Indirizzo email dell'utente.
+     */
+
     private String email;
+
+    /**
+     * Token strutturato secondo lo schema {email}:{sha256(concat(email, password))}.
+     */
+
     private String token;
+
+    /**
+     * Crea un nuovo oggetto AuthToken a partire da un utente.
+     *
+     * @param user L'utente per il quale viene generato il token di autenticazione.
+     */
 
     AuthToken(User user) {
         this.email = user.getEmail();
@@ -26,14 +60,33 @@ public class AuthToken {
         this.token = email + "::" + Util.sha256(s);
     }
 
+    /**
+     * Crea un nuovo oggetto AuthToken a partire da un token.
+     *
+     * @param token Il token fornito per la costruzione.
+     */
+
     public AuthToken(String token) {
         this.email = token.split("::")[0];
         this.token = token;
     }
 
+    /**
+     * Metodo "getter" per ottenere il token.
+     *
+     * @return Il token di autenticazione.
+     */
+
     public String getToken() {
         return token;
     }
+
+    /**
+     * Verifica che un AuthToken sia valido per un dato User.
+     *
+     * @param u User per il quale si vuole valutare la validità del token.
+     * @return True se l'AuthToken è valido per l'utente, False altrimenti.
+     */
 
     public boolean validFor(User u) {
         return u.getAuthToken().getToken().equals(getToken());
