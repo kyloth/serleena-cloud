4b28634969e9ac4b6289dbfa42a328e41de5bb50
Gabriele Pozzan <fanofthepigpen@gmail.com>
2015-05-13 10:27:30 +0200

    JAVADOC: aggiungi javadoc a River
    Related to: SHCLOUD-11

--- src/main/java/com/hitchhikers/serleenacloud/datamodel/business/River.java
+++ src/main/java/com/hitchhikers/serleenacloud/datamodel/business/River.java
@@ -13,24 +13,65 @@
 ******************************************************************************/
 
 
+/**
+ * Name: River.java
+ * Package: com.kyloth.serleenacloud.datamodel.business
+ * Author: Nicola Mometto
+ *
+ * History:
+ * Version  Programmer      Changes
+ * 1.0.0    Nicola Mometto  Creazione file, codice e javadoc iniziali
+ */
+
 package com.kyloth.serleenacloud.datamodel.business;
 
 import com.kyloth.serleenacloud.datamodel.geometry.AWideLine;
 import com.kyloth.serleenacloud.datamodel.geometry.IWeighedPoint;
 
+/**
+ * Classe che rappresenta un'entità di mappa di tipo fiume.
+ *
+ * @author Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ */
+
 public class River extends AWideLine {
 
+    /**
+     * Il nome del fiume.
+     */
+
     private String name;
+
+    /**
+     * Crea un nuovo oggetto River inizializzandone i campi dati.
+     *
+     * @param points Insieme di IWeighedPoint che definiscono il fiume.
+     * @param name Il nome del fiume.
+     */
 
     public River(Iterable<IWeighedPoint> points, String name) {
         super(points);
         this.name = name;
     }
 
+    /**
+     * Crea un nuovo oggetto River inizializzandone i campi dati.
+     *
+     * @param points Array di IWeighedPoint che definiscono il fiume.
+     * @param name Il nome del fiume.
+     */
+
     public River(IWeighedPoint[] points, String name) {
         super(points);
         this.name = name;
     }
+
+    /**
+     * Metodo "getter" per ottenere il nome del fiume.
+     *
+     * @return Restituisce il nome del fiume.
+     */
 
     public String getName() {
         return name;
