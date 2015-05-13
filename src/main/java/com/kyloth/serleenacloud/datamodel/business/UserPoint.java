5d87a019250b517e8c12065157c725a9254eaf6e
Gabriele Pozzan <nicola.mometto@studenti.unipd.it>
2015-05-13 10:28:55 +0200

    JAVADOC: aggiungi javadoc a UserPoint
    Related to: SHCLOUD-11

--- src/main/java/com/kyloth/serleenacloud/datamodel/business/UserPoint.java
+++ src/main/java/com/kyloth/serleenacloud/datamodel/business/UserPoint.java
@@ -13,18 +13,53 @@
 ******************************************************************************/
 
 
+/**
+ * Name: UserPoint.java
+ * Package: com.kyloth.serleenacloud.datamodel.business
+ * Author: Nicola Mometto
+ *
+ * History:
+ * Version  Programmer      Changes
+ * 1.0.0    Nicola Mometto  Creazione file, codice e javadoc iniziali
+ */
+
 package com.kyloth.serleenacloud.datamodel.business;
 
 import com.kyloth.serleenacloud.datamodel.geometry.Point;
 
+/**
+ * Classe che rappresente un Punto Utente in un'Esperienza.
+ *
+ * @author Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ */
+
 public class UserPoint extends Point {
 
+    /**
+     * Nome del Punto Utente.
+     */
+
     String name;
+
+    /**
+     * Crea un nuovo Punto Utente inizializzandone i campi dati.
+     *
+     * @param latitude Latitudine del Punto Utente.
+     * @param longitude Longitudine del Punto Utente.
+     * @param name Nome del Punto Utente.
+     */
 
     public UserPoint(double latitude, double longitude, String name) {
         super(latitude, longitude);
         this.name = name;
     }
+
+    /**
+     * Metodo "getter" per ottenere il nome del Punto Utente.
+     *
+     * @return Restituisce il nome del Punto Utente.
+     */
 
     public String getName() {
         return name;
