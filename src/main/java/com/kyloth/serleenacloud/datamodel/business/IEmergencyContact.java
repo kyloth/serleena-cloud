cfeb44666498a2ea0df32b7d8e59a6cb1e6f5e32
Gabriele Pozzan <nicola.mometto@studenti.unipd.it>
2015-05-13 10:24:40 +0200

    JAVADOC: aggiungi javadoc a IEmergencyContact
    Related to: SHCLOUD-11

--- src/main/java/com/kyloth/serleenacloud/datamodel/business/IEmergencyContact.java
+++ src/main/java/com/kyloth/serleenacloud/datamodel/business/IEmergencyContact.java
@@ -13,12 +13,51 @@
 ******************************************************************************/
 
 
+/**
+ * Name: IEmergencyContact.java
+ * Package: com.kyloth.serleenacloud.datamodel.business
+ * Author: Nicola Mometto
+ *
+ * History:
+ * Version  Programmer      Changes
+ * 1.0.0    Nicola Mometto  Creazione file, codice e javadoc iniziali
+ */
+
 package com.kyloth.serleenacloud.datamodel.business;
 
 import com.kyloth.serleenacloud.datamodel.geometry.IRect;
 
+/**
+ * Interfaccia che rappresenta informazioni riguardanti contatti di emergenza
+ * disponibili in un'area di mappa.
+ *
+ * @author Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ */
+
 public interface IEmergencyContact {
+
+    /**
+     * Metodo "getter" che permette di ottenere un oggetto IRect che rappresenta un'area geografica.
+     *
+     * @return Restituisce un oggetto IRect che rappresenta l'area geografica relativa al contatto di emergenza.
+     */
+
     public IRect getBoundingRect();
+
+    /**
+     * Metodo "getter" che permette di ottenere il nome del contatto di emergenza.
+     *
+     * @return Restituisce il nome del contatto di emergenza.
+     */
+
     public String getName();
+
+    /**
+     * Metodo "getter" che permette di ottenere il numero del contatto di emergenza.
+     *
+     * @return Restituisce il numero del contatto di emergenza.
+     */
+
     public String getNumber();
 }
