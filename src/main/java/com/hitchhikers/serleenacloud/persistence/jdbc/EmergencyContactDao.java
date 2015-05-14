d3db66f295bdb6860e254a891ec0cc743c65868b
Nicola Mometto <nicola.mometto@studenti.unipd.it>
2015-05-14 04:43:28 +0200

    PERSISTENCE: Aggiungi EmergencyContactDao
    Related to: SHCLOUD-10

---
+++ src/main/java/com/hitchhikers/serleenacloud/persistence/jdbc/EmergencyContactDao.java
@@ -0,0 +1,65 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.persistence.jdbc;
+
+import com.kyloth.serleenacloud.datamodel.business.IEmergencyContact;
+import com.kyloth.serleenacloud.datamodel.business.EmergencyContact;
+import com.kyloth.serleenacloud.datamodel.geometry.IRect;
+import com.kyloth.serleenacloud.datamodel.geometry.Rect;
+import com.kyloth.serleenacloud.datamodel.geometry.Point;
+import com.kyloth.serleenacloud.persistence.IEmergencyContactDao;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class EmergencyContactDao implements IEmergencyContactDao {
+
+    private JdbcTemplate tpl;
+
+    EmergencyContactDao(JDBCDataSource ds) {
+        this.tpl = ds.getTpl();
+    }
+
+    public Iterable<IEmergencyContact> findAll(IRect region) {
+        return tpl.query("SELECT nwlatitude, nwlongitude, selatitude, selongitude, name, number " +
+                         "FROM emergencycontacts " +
+                         "WHERE ((nwlatitude BETWEEN ? AND ?) AND (nwlongitude BETWEEN ? AND ?))" +
+                         "OR ((selatitude BETWEEN ? AND ?) AND (selongitude BETWEEN ? AND ?))",
+                         new Object[] {
+                             region.getNWPoint().getLatitude(),
+                             region.getSEPoint().getLatitude(),
+                             region.getNWPoint().getLongitude(),
+                             region.getSEPoint().getLongitude(),
+                             region.getNWPoint().getLatitude(),
+                             region.getSEPoint().getLatitude(),
+                             region.getNWPoint().getLongitude(),
+                             region.getSEPoint().getLongitude()},
+                         new RowMapper<IEmergencyContact>() {
+                             @Override
+                             public IEmergencyContact mapRow(ResultSet rs, int rowNum) throws SQLException {
+                                 return new EmergencyContact(rs.getString("name"),
+                                                             new Rect(new Point(rs.getDouble("nwlatitude"),
+                                                                                rs.getDouble("nwlongitude")),
+                                                                      new Point(rs.getDouble("selatitude"),
+                                                                                rs.getDouble("selongitude"))),
+                                                             rs.getString("number"));
+                             }
+                         });
+    }
+}
