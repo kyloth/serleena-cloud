86b94e4f33523c7c8a14673e34069c203a016afa
Nicola Mometto <nicola.mometto@studenti.unipd.it>
2015-05-22 23:54:07 +0200

    CONTROLLER: Aggiungi User/recovery, stub email

---
+++ src/main/java/com/hitchhikers/serleenacloud/controller/Utils.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+* Copyright (c) 2015 Nicola Mometto
+* All rights reserved. This program and the accompanying materials
+* are made available under the terms of the Eclipse Public License v1.0
+* which accompanies this distribution, and is available at
+* http://www.eclipse.org/legal/epl-v10.html
+*
+* Contributors:
+*  Nicola Mometto
+*  Antonio Cavestro
+*  Sebastiano Valle
+*  Gabriele Pozzan
+******************************************************************************/
+
+
+package com.kyloth.serleenacloud.controller;
+
+public class Utils {
+
+    public static void sendMail(String to, String subj, String body) {
+        // TODO
+    }
+
+}
