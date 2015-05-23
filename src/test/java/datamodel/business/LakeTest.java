fdf2a5d3d0bfa02eed298526e226cc8bc8b745bd
Gabriele Pozzan <nicola.mometto@studenti.unipd.it>
2015-05-23 09:43:49 +0200

    RM: aggiusta riferimenti test business
    Related to: SHCLOUD-15

--- src/test/java/datamodel/business/LakeTest.java
+++ src/test/java/datamodel/business/LakeTest.java
@@ -28,8 +28,6 @@ package com.kyloth.serleenacloud.datamodel.business;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import com.kyloth.serleenacloud.datamodel.geometry.IPoly;
-import com.kyloth.serleenacloud.datamodel.geometry.IPoint;
 import com.kyloth.serleenacloud.datamodel.geometry.Point;
 import java.util.Arrays;
 import java.util.Iterator;
@@ -51,26 +49,26 @@ public class LakeTest {
         String name = "Loch Ness";
         double[] latitudes = {3.2, 15.6, -18.9, 47.01};
         double[] longitudes = {6.23, 99.33, -55.03, 14};
-        IPoint[] points = {new Point(latitudes[0], longitudes[0]),
+        Point[] points = {new Point(latitudes[0], longitudes[0]),
                    new Point(latitudes[1], longitudes[1]),
                    new Point(latitudes[2], longitudes[2]),
                    new Point(latitudes[3], longitudes[3])
         };
-        Iterable<IPoint> i_points = Arrays.asList(points);
+        Iterable<Point> i_points = Arrays.asList(points);
         Lake l_1 = new Lake(i_points, name);
         Lake l_2 = new Lake(points, name);
-        Iterable<IPoint> l_1_points = l_1.getPoints();
-        Iterable<IPoint> l_2_points = l_2.getPoints();
-        Iterator<IPoint> l_1_iterator = l_1_points.iterator();
-        Iterator<IPoint> l_2_iterator = l_2_points.iterator();
-        Iterator<IPoint> input_iterator = i_points.iterator();
+        Iterable<Point> l_1_points = l_1.getPoints();
+        Iterable<Point> l_2_points = l_2.getPoints();
+        Iterator<Point> l_1_iterator = l_1_points.iterator();
+        Iterator<Point> l_2_iterator = l_2_points.iterator();
+        Iterator<Point> input_iterator = i_points.iterator();
 
         assertTrue(l_1.getName().equals("Loch Ness"));
         while(l_1_iterator.hasNext() && l_2_iterator.hasNext()
                 && input_iterator.hasNext()) {
-            IPoint l_1_point = l_1_iterator.next();
-            IPoint l_2_point = l_2_iterator.next();
-            IPoint input_point = input_iterator.next();
+            Point l_1_point = l_1_iterator.next();
+            Point l_2_point = l_2_iterator.next();
+            Point input_point = input_iterator.next();
             assertTrue(l_1_point.getLatitude() == input_point.getLatitude());
             assertTrue(l_2_point.getLatitude() == input_point.getLatitude());
             assertTrue(l_1_point.getLongitude() == input_point.getLongitude());
