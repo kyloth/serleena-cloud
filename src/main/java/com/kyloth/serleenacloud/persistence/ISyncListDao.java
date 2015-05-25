c85ac2172564ede6d55930b9dd6b29a6ec718544
Gabriele Pozzan <fanofthepigpen@gmail.com>
2015-05-25 18:00:59 +0200

    JAVADOC: aggiungi javadoc package persistence
    Related to: SHCLOUD-11

--- src/main/java/com/kyloth/serleenacloud/persistence/ISyncListDao.java
+++ src/main/java/com/kyloth/serleenacloud/persistence/ISyncListDao.java
@@ -13,11 +13,46 @@
 ******************************************************************************/
 
 
+/**
+ * Name: ISyncListDao.java
+ * Package: com.kyloth.serleenacloud.persistence
+ * Author: Nicola Mometto
+ *
+ * History:
+ * Version  Programmer      Changes
+ * 1.0.0    Nicola Mometto  Creazione file, codice e javadoc iniziali
+ */
+
 package com.kyloth.serleenacloud.persistence;
 
 import com.kyloth.serleenacloud.datamodel.business.Experience;
 
+/**
+ * Interfaccia implementata da una classe che realizza la persistenza su database della lista delle esperienze da sincronizzare.
+ *
+ * @use Contiene metodi per cercare e inserire nuove esperienze nella lista di sincronizzazione della base di dati.
+ *
+ * @author Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ */
+
 public interface ISyncListDao {
+    
+    /**
+     * Permette di inserire una nuova Esperienza nella lista di
+     * sincronizzazione.
+     *
+     * @param es L'Esperienza da inserire nella lista.
+     */
+
     public void persist(Iterable<Experience> es);
+    
+    /**
+     * Permette di ottenere tutte le Esperienze in lista di
+     * sincronizzazione.
+     *
+     * @return Restituisce la lista delle Esperienze in lista di sincronizzazione.
+     */
+
     public Iterable<Experience> findAll();
 }
--- src/main/java/com/kyloth/serleenacloud/persistence/IWeatherForecastDao.java
+++ src/main/java/com/kyloth/serleenacloud/persistence/IWeatherForecastDao.java
@@ -13,6 +13,16 @@
 ******************************************************************************/
 
 
+/**
+ * Name: IWeatherForecastDao.java
+ * Package: com.kyloth.serleenacloud.persistence
+ * Author: Nicola Mometto
+ *
+ * History:
+ * Version  Programmer      Changes
+ * 1.0.0    Nicola Mometto  Creazione file, codice e javadoc iniziali
+ */
+
 package com.kyloth.serleenacloud.persistence;
 
 import com.kyloth.serleenacloud.datamodel.business.WeatherForecast;
@@ -20,6 +30,26 @@ import com.kyloth.serleenacloud.datamodel.geometry.Rect;
 
 import java.util.Date;
 
+/**
+ * Interfaccia implementata da una classe che realizza la persistenza su database degli oggetti di tipo WeatherForecast.
+ *
+ * @use Contiene metodi per cercare informazioni meteo nella base di dati.
+ *
+ * @author Nicola Mometto <nicola.mometto@studenti.unipd.it>
+ * @version 1.0
+ */
+
 public interface IWeatherForecastDao {
+    
+    /**
+     * Permette di trovare tutte le informazioni meteo relative a una
+     * certa regione, per un certo intervallo di tempo.
+     *
+     * @param region La regione di interesse.
+     * @param start Inizio dell'intervallo temporale di interesse.
+     * @param end Fine dell'intervallo temporale di interesse.
+     * @return Restituisce la lista delle informazioni meteo in base ai parametri specificati.
+     */
+
     Iterable<WeatherForecast> findAll(Rect region, Date start, Date end);
 }
