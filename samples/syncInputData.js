1ad39b61623de7e740508abd84b2038fa25dd155
Nicola Mometto <nicola.mometto@studenti.unipd.it>
2015-07-21 21:38:59 +0200

    SAMPLE: aggiungi syncInputData.js

---
+++ samples/syncInputData.js
@@ -0,0 +1,16 @@
+[{
+    "experience" : "Experience_1",
+    "userPoints" : [ {
+        "latitude" : 4.0,
+        "longitude" : 4.0,
+        "name" : "UP1"
+    }],
+    telemetryData : [ {
+        "events" : [ {
+            "type" : "CHECKPOINT",
+            "time" : 1437505820522,
+            "value" : 100.0
+        } ],
+        "track" : "Track_1"
+    }]
+}]
