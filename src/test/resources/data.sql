98f87139d94febf4a2c58ef48e08a49c4e633136
Nicola Mometto <brobronsa@gmail.com>
2015-07-23 20:18:38 +0200

    DATA: aggiungi sentiero d'esempio

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -1,3 +1,14 @@
 INSERT INTO POIs (Name, Longitude, Latitude, Type) VALUES ('POI1', 6, 6, 'INFO'), ('POI2', 7, 7, 'FOOD');
 
 INSERT INTO EmergencyContacts (Name, Number, NWLongitude, NWLatitude, SELongitude, SELatitude) VALUES ('Emergency_1', 1, 1, 10, 10, 1);
+
+INSERT INTO Paths (Name) VALUES ('foo');
+
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.276413, 11.650587, 1, 0);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.275977, 11.652806, 1, 1);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.276257, 11.654908, 1, 2);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.277573, 11.654297, 1, 3);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.278192, 11.655213, 1, 4);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.279032, 11.655626, 1, 5);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.280266, 11.655875, 1, 6);
+INSERT INTO PathPoints (PathName, Latitude, Longitude, Radius, Idx) VALUES ('foo', 45.281726, 11.655550, 1, 7);
