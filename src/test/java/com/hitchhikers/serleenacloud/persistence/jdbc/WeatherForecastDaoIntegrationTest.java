2a4e700f60f9320f47e326b2e101d86ed7777a31
gpozzan <fanofthepigpen@gmail.com>
2015-08-18 22:04:55 +0200

    PERSISTENCE/TEST: aggiungi test per WeatherForecastDaoIntegrationTest
    Related to: SHCLOUD-49

--- src/test/java/com/hitchhikers/serleenacloud/persistence/jdbc/WeatherForecastDaoIntegrationTest.java
+++ src/test/java/com/hitchhikers/serleenacloud/persistence/jdbc/WeatherForecastDaoIntegrationTest.java
@@ -70,7 +70,9 @@ public class WeatherForecastDaoIntegrationTest {
         ds = (JDBCDataSource) context.getBean("dataSource");
         tpl = ds.getTpl();
         String insertWeatherForecast = "INSERT INTO WeatherForecasts (MTemperature, ATemperature, NTemperature, Date, MForecast, AForecast, NForecast, NWLongitude, NWLatitude, SELongitude, SELatitude) VALUES (12, 12, 12, '1980-01-01 00:00:01', 'CLOUDY', 'CLOUDY', 'CLOUDY', 6, 15, 15, 6), (13, 13, 13, '1975-01-01 00:00:01', 'SUNNY', 'SUNNY', 'SUNNY', 6, 15, 15, 6), (14, 14, 14, '1980-01-01 00:00:01','SNOWY', 'SNOWY', 'SNOWY', 30, 20, 20, 30);";
+        String insertWeatherForecast1 = "INSERT INTO WeatherForecasts (MTemperature, ATemperature, NTemperature, Date, MForecast, AForecast, NForecast, NWLongitude, NWLatitude, SELongitude, SELatitude) VALUES (50, 50, 50, '1980-01-01 00:00:01', 'RAINY', 'RAINY', 'RAINY', 11.646284, 45.277693, 11.650876, 45.274461);";
         tpl.update(insertWeatherForecast);
+        tpl.update(insertWeatherForecast1);
         wfd = ds.weatherForecastDao();
     }
 
@@ -98,8 +100,72 @@ public class WeatherForecastDaoIntegrationTest {
         Iterable<WeatherForecast> forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
         Iterator<WeatherForecast> i_forecasts = forecasts.iterator();
         WeatherForecast wf = i_forecasts.next();
-        assertFalse(i_forecasts.hasNext());
         assertTrue(wf.getMorning().getForecast() == WeatherForecast.WeatherCondition.CLOUDY);
+    }
 
+    @Test
+    public void testFindAllRealDataHitRegion() {
+        long timestamp_1 = 284043191000L;
+        long timestamp_2 = 347201591000L;
+        //Punto NW della regione cade all'interno della regione delle previsioni
+        Rect region = new Rect(new Point(45.275700, 11.649052), new Point(45.273540, 11.656369));
+        Iterable<WeatherForecast> forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        Iterator<WeatherForecast> i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto SE della regione cade all'interno della regione delle previsioni
+        region = new Rect(new Point(45.279912, 11.643151), new Point(45.275745, 11.649224));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto NE della regione cade all'interno della regione delle previsioni
+        region = new Rect(new Point(45.275866, 11.639632), new Point(45.269483, 11.649230));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto SW della regione cade all'interno della regione delle previsioni
+        region = new Rect(new Point(45.281281, 11.648720), new Point(45.275427, 11.659335));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Regione completamente contenuta in quella delle previsioni
+        region = new Rect(new Point(45.275876, 11.648643), new Point(45.275230, 11.649536));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto NW della regione delle previsioni cade all'interno della regione
+        region = new Rect(new Point(45.278123, 11.645123), new Point(45.276123, 11.647123));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto SE della regione delle previsioni cade all'interno della regione
+        region = new Rect(new Point(45.277512, 11.647123), new Point(45.265123, 11.660123));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto NE della regione delle previsioni cade all'interno della regione
+        region = new Rect(new Point(45.275123, 11.630123), new Point(45.265123, 11.648123));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Punto SW della regione delle previsioni cade all'interno della regione
+        region = new Rect(new Point(45.279123, 11.649876), new Point(45.276834, 11.651647));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
+        //Regione delle previsioni completamente contenuta nella regione
+        region = new Rect(new Point(45.278656, 11.639536), new Point(45.273758, 11.650991));
+        forecasts = wfd.findAll(region, new Date(timestamp_1), new Date(timestamp_2));
+        i_forecasts = forecasts.iterator();
+        assertTrue(i_forecasts.next().getMorning().getForecast() == WeatherForecast.WeatherCondition.RAINY);
+        assertFalse(i_forecasts.hasNext());
     }
 }
